25a853c9507f2d186b7a2c15aa7d4cea519bf1ef
Poncio Elias Rodrigues de Oliveira <poncio@teltelecom.com.br>
2026-02-25 12:19:40 -0300

    add config pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
 import {
   ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
+  provideBrowserGlobalErrorListeners, isDevMode,
 } from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 
 import {routes} from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), 
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 };
